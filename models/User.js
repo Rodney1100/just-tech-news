8c3eba65521fdf15244373777d00d6e7f0d9e99f
Rodney P <rodney.plancher@gmail.com>
2022-02-18 23:42:38 -0500

    ability to add, get and subtract users

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+//  Create user model
+class User extends Model {}
+
+// Define table columns and configuration
+User.init(
+  {
+    // TABLE COLUMN DEFINITIONS GO HERE
+    id: {
+      // use the special sequelize datatype object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivilate of sql's not null
+      allowNull: false,
+      // instruct that this is the primary key
+      primaryKey: true,
+      // turn on auto incramant
+      autoIncrement: true,
+    },
+    // define username column
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    // define password column
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // this means the password must be at least four characters long
+        len: [4],
+      },
+    },
+  },
+
+  {
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: "user",
+  }
+);
+module.exports = User;
