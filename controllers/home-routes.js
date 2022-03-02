37a1124d68292fb3aa91e637beefdeb6ae064cfe
Rodney P <rodney.plancher@gmail.com>
2022-03-02 16:11:22 -0500

    added ses  for login and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
+const { Post, User, Comment, Vote } = require("../models");
 router.get("/", (req, res) => {
+  console.log("===========================");
   Post.findAll({
     attributes: [
       "id",
@@ -33,6 +34,14 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
