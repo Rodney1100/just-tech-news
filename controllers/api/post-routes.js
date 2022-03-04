35b156faae4862199d5b5c9bca7bd01efebe0b89
Rodney P <rodney.plancher@gmail.com>
2022-03-04 16:11:42 -0500

    visual upvote and creating comments

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,22 +87,23 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
-  Post.create({
-    title: req.body.title,
-    post_url: req.body.post_url,
-    user_id: req.session.user_id,
-  })
-    .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  if (req.session) {
+    Post.create({
+      title: req.body.title,
+      post_url: req.body.post_url,
+      user_id: req.session.user_id,
+    })
+      .then((dbPostData) => res.json(dbPostData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put("/upvote", (req, res) => {
   // custom static method created in models/Post.js
   if (req.session) {
-    // pass session id along with all destructured properties on req.body
     Post.upvote(
       { ...req.body, user_id: req.session.user_id },
       { Vote, Comment, User }
@@ -111,23 +112,6 @@ router.put("/upvote", (req, res) => {
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
-      });
-  }
-});
-
-router.post("/", (req, res) => {
-  //  check the session
-  if (req.session) {
-    Comment.create({
-      comment_text: req.body.comment_text,
-      post_id: req.body.post_id,
-      // use the id from the session
-      user_id: req.session.user_id,
-    })
-      .then((dbCommentData) => res.json(dbCommentData))
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json(err);
       });
   }
 });
