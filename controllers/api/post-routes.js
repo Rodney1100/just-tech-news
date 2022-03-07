cf39e9cd6f64e6bad5f88cd963b4d53108dcb517
Rodney P <rodney.plancher@gmail.com>
2022-03-07 17:29:07 -0500

    mvp finished

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
+
 const { Post, User, Comment, Vote } = require("../../models");
 
 // get all users
@@ -85,7 +87,7 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", withAuth, (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   if (req.session) {
     Post.create({
@@ -101,7 +103,7 @@ router.post("/", (req, res) => {
   }
 });
 
-router.put("/upvote", (req, res) => {
+router.put("/upvote", withAuth, (req, res) => {
   // custom static method created in models/Post.js
   if (req.session) {
     Post.upvote(
@@ -116,7 +118,7 @@ router.put("/upvote", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -140,7 +142,7 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", withAuth, (req, res) => {
   console.log("id", req.params.id);
   Post.destroy({
     where: {
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+const router = require("express").Router();
+const dashboardRoutes = require("./dashboard-routes.js");
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
+router.use("/dashboard", dashboardRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
